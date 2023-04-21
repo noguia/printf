49bfbb6472a85438acbaa46a06c83a80325f0dd9
noguia <noguiaisback@gmail.com>
2023-04-21 08:48:01 -0700

    good start

---
+++ main.h
@@ -0,0 +1,17 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+
+#include <unistd.h>
+#include <stdarg.h>
+#include <math.h>
+
+
+long _tobinary(unsigned int x);
+int _putchar(char c);
+char _putstr(char *s;
+int _putnbr(int n);
+int _printf(const char *format, ...);
+
+
+#endif
