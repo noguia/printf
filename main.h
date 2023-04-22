9ac1fcba655438599fa8dca51e775de6d758dfd1
noguia <noguiaisback@gmail.com>
2023-04-22 04:53:47 -0700

    I'm here and I'm a Spur for life

--- main.h
+++ main.h
@@ -1,12 +1,27 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-#include <stdlib.h>
 #include <stdarg.h>
-#include <string.h>
-#include <unistd.h>
 
-int _putchar(char s);
-int _printf(const char *format, ...);
+/* ***for write*** */
+
+#include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+
+
+#define BUFF_SIZE 1024
+
+/* ****************Prototype******************* */
+
+int		 _printf(const char *format, ...);
+int	 _putchar(char c);
+int		 _strlen(char *s);
+int	 _print_str(char *str);
+int		 _print_bin(unsigned int n);
+int	 _print_unsigned(unsigned int n, int base, char *digit);
+int		 _print_int(int n);
 
 #endif
+
